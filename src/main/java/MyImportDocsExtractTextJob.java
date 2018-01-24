39a65de94e393df253ee281831d45def6a364c23
Nishad Bakshi <nishadbakshi@gmail.com>
2018-01-24 17:39:09 +1100

    clean up code by using spring-batch-file dependency

--- src/main/java/MyImportDocsExtractTextJob.java
+++ src/main/java/MyImportDocsExtractTextJob.java
@@ -1,22 +1,11 @@
 
-import readers.EnhancedResourcesItemReader;
 import com.marklogic.client.DatabaseClient;
 import com.marklogic.client.document.DocumentWriteOperation;
 import com.marklogic.client.ext.helper.DatabaseClientProvider;
-import com.marklogic.client.impl.DocumentWriteOperationImpl;
-import com.marklogic.client.io.DOMHandle;
-import com.marklogic.client.io.DocumentMetadataHandle;
-import com.marklogic.client.io.Format;
-import com.marklogic.client.io.StringHandle;
-import com.marklogic.client.io.marker.AbstractWriteHandle;
-import com.marklogic.client.io.marker.DocumentMetadataWriteHandle;
-import com.marklogic.spring.batch.item.processor.MarkLogicItemProcessor;
 import com.marklogic.spring.batch.item.writer.MarkLogicItemWriter;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.StringReader;
+import com.marklogic.spring.batch.item.file.EnhancedResourcesItemReader;
+import com.marklogic.spring.batch.item.file.TikaParserItemProcessor;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.*;
@@ -25,41 +14,14 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 
-import java.util.UUID;
-import java.util.stream.Collectors;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.parser.AutoDetectParser;
-import org.apache.tika.sax.BodyContentHandler;
-import org.apache.tika.sax.ToXMLContentHandler;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.io.Resource;
-import org.w3c.dom.Document;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.InputSource;
 
-/**
- * YourJobConfig.java - a Spring Batch configuration template that demonstrates
- * ingesting data into MarkLogic. This job specification uses the
- * MarkLogicBatchConfiguration that utilizes a MarkLogic implementation of a
- * JobRepository.
- *
- * @author Scott Stafford
- * @version 1.4.0
- * @see EnableBatchProcessing
- * @see com.marklogic.spring.batch.config.MarkLogicBatchConfiguration
- * @see com.marklogic.spring.batch.config.MarkLogicConfiguration
- * @see com.marklogic.spring.batch.item.processor.MarkLogicItemProcessor
- * @see com.marklogic.spring.batch.item.writer.MarkLogicItemWriter
- */
 @EnableBatchProcessing
 @Import(value = {
     com.marklogic.spring.batch.config.MarkLogicBatchConfiguration.class,
@@ -83,12 +45,14 @@ public class MyImportDocsExtractTextJob {
      * @return Job
      */
     @Bean(name = JOB_NAME)
-    public Job job(JobBuilderFactory jobBuilderFactory, Step step) {
+    public Job job(
+            JobBuilderFactory jobBuilderFactory,
+            @Qualifier("loadDocumentsFromDirectoryJobStep1") Step step) {
+
         JobExecutionListener listener = new JobExecutionListener() {
             @Override
             public void beforeJob(JobExecution jobExecution) {
                 logger.info("BEFORE JOB");
-                jobExecution.getExecutionContext().putString("random", "yourJob123");
             }
 
             @Override
@@ -104,61 +68,18 @@ public class MyImportDocsExtractTextJob {
                 .build();
     }
 
-    /**
-     * The StepBuilderFactory and DatabaseClientProvider parameters are injected
-     * via Spring. Custom parameters must be annotated with @Value.
-     *
-     * @param stepBuilderFactory injected from the @EnableBatchProcessing
-     * annotation
-     * @param databaseClientProvider injected from the BasicConfig class
-     * @param collections This is an example of how user parameters could be
-     * injected via command line or a properties file
-     * @return Step
-     * @see DatabaseClientProvider
-     * @see ItemReader
-     * @see ItemProcessor
-     * @see DocumentWriteOperation
-     * @see MarkLogicItemProcessor
-     * @see MarkLogicItemWriter
-     */
     @Bean
     @JobScope
-    public Step step(
+    public Step loadDocumentsFromDirectoryJobStep1(
             StepBuilderFactory stepBuilderFactory,
             DatabaseClientProvider databaseClientProvider,
             @Value("#{jobParameters['input_file_path']}") String inputFilePath,
             @Value("#{jobParameters['input_file_pattern']}") String inputFilePattern) {
 
-        ItemProcessor<Resource, DocumentWriteOperation> processor = new ItemProcessor<Resource, DocumentWriteOperation>() {
-            @Override
-            public DocumentWriteOperation process(Resource item) throws Exception {
-                ContentHandler handler = new ToXMLContentHandler();
+        EnhancedResourcesItemReader itemReader = new EnhancedResourcesItemReader(inputFilePath, inputFilePattern);
 
-                AutoDetectParser parser = new AutoDetectParser();
-                Metadata metadata = new Metadata();
-
-                logger.info("processing: " + item.getFilename());
-
-                InputStream stream = item.getInputStream();
-
-                parser.parse(stream, handler, metadata);
-
-                logger.info("content found: " + handler.toString());
-
-                DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                DocumentBuilder builder = factory.newDocumentBuilder();
-
-                Document document = builder.parse(new InputSource(new StringReader(handler.toString())));
-                document.setDocumentURI(item.getFilename());
-                DOMHandle handle = new DOMHandle(document);
-                handle.setFormat(Format.XML);
-                return new DocumentWriteOperationImpl(
-                        DocumentWriteOperation.OperationType.DOCUMENT_WRITE,
-                        item.getFilename() + ".xml",
-                        new DocumentMetadataHandle().withCollections("binary"),
-                        handle);
-            }
-        };
+        TikaParserItemProcessor itemProcessor = new TikaParserItemProcessor();
+        itemProcessor.setCollections(new String[]{"directoryJob"});
 
         DatabaseClient databaseClient = databaseClientProvider.getDatabaseClient();
 
@@ -167,23 +88,10 @@ public class MyImportDocsExtractTextJob {
 
         return stepBuilderFactory.get("step1")
                 .<Resource, DocumentWriteOperation>chunk(10)
-                .reader(new EnhancedResourcesItemReader(inputFilePath, inputFilePattern))
-                .processor(processor)
+                .reader(itemReader)
+                .processor(itemProcessor)
                 .writer(writer)
                 .build();
-    }
-
-    public static String streamToString(final InputStream inputStream) throws Exception {
-        // buffering optional
-        try (
-                final BufferedReader br
-                = new BufferedReader(new InputStreamReader(inputStream))) {
-            // parallel optional
-            return br.lines().parallel().collect(Collectors.joining("\n"));
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
-            // whatever.
-        }
     }
 
 }
