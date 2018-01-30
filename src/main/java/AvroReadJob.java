d1ab3c45fe9ff77f51eb2058e867183ada291388
Nishad Bakshi <nishadbakshi@gmail.com>
2018-01-30 19:10:10 +1100

    add a sample avro job

---
+++ src/main/java/AvroReadJob.java
@@ -0,0 +1,169 @@
+import com.marklogic.client.DatabaseClient;
+import com.marklogic.client.document.DocumentWriteOperation;
+import com.marklogic.client.ext.helper.DatabaseClientProvider;
+import com.marklogic.client.io.DocumentMetadataHandle;
+import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.io.marker.AbstractWriteHandle;
+import com.marklogic.client.io.marker.DocumentMetadataWriteHandle;
+import com.marklogic.spring.batch.item.processor.MarkLogicItemProcessor;
+import com.marklogic.spring.batch.item.writer.MarkLogicItemWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@EnableBatchProcessing
+@Import(value = {
+        com.marklogic.spring.batch.config.MarkLogicBatchConfiguration.class,
+        com.marklogic.spring.batch.config.MarkLogicConfiguration.class})
+@PropertySource("classpath:job.properties")
+public class AvroReadJob {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    // This is the bean label for the name of your Job.  Pass this label into the job_id parameter
+    // when using the CommandLineJobRunner
+    private final String JOB_NAME = "myAvroJob";
+
+    /**
+     * The JobBuilderFactory and Step parameters are injected via the
+     * EnableBatchProcessing annotation.
+     *
+     * @param jobBuilderFactory injected from the @EnableBatchProcessing
+     *                          annotation
+     * @param step              injected from the step method in this class
+     * @return Job
+     */
+    @Bean(name = JOB_NAME)
+    public Job job(
+            JobBuilderFactory jobBuilderFactory,
+            @Qualifier("loadDocumentsFromDirectoryJobStep1") Step step) {
+
+        JobExecutionListener listener = new JobExecutionListener() {
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                logger.info("BEFORE JOB");
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+                logger.info("AFTER JOB");
+            }
+        };
+
+        return jobBuilderFactory.get(JOB_NAME)
+                .start(step)
+                .listener(listener)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step loadDocumentsFromDirectoryJobStep1(
+            StepBuilderFactory stepBuilderFactory,
+            DatabaseClientProvider databaseClientProvider,
+            @Value("#{jobParameters['output_collections'] ?: 'avro'}") String[] collections,
+            @Value("#{jobParameters['input_file']}") String input_file) throws IOException {
+
+
+        ItemReader<String> reader = new ItemReader<String>() {
+
+
+            //initialize
+            File avroFile = new File(input_file);
+            DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+            DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(avroFile, datumReader);
+
+            GenericRecord record = null;
+
+            @Override
+            public String read() throws Exception {
+
+                if (dataFileReader.hasNext()) {
+                    record = dataFileReader.next(record);
+                    //System.out.println(record);
+                    return record.toString();
+                } else {
+                    return null;
+                }
+
+            }
+        };
+
+        //The ItemProcessor is typically customized for your Job.  An anoymous class is a nice way to instantiate but
+        //if it is a reusable component instantiate in its own class
+        MarkLogicItemProcessor<String> processor = new MarkLogicItemProcessor<String>() {
+
+            @Override
+            public DocumentWriteOperation process(String item) throws Exception {
+                DocumentWriteOperation dwo = new DocumentWriteOperation() {
+
+                    @Override
+                    public OperationType getOperationType() {
+                        return OperationType.DOCUMENT_WRITE;
+                    }
+
+                    @Override
+                    public String getUri() {
+                        return UUID.randomUUID().toString() + ".json";
+                    }
+
+                    @Override
+                    public DocumentMetadataWriteHandle getMetadata() {
+                        DocumentMetadataHandle metadata = new DocumentMetadataHandle();
+                        metadata.withCollections(collections);
+                        return metadata;
+                    }
+
+                    @Override
+                    public AbstractWriteHandle getContent() {
+                        return new StringHandle(String.format(item));
+                    }
+
+                    @Override
+                    public String getTemporalDocumentURI() {
+                        return null;
+                    }
+                };
+                return dwo;
+            }
+        };
+
+        DatabaseClient databaseClient = databaseClientProvider.getDatabaseClient();
+
+        MarkLogicItemWriter writer = new MarkLogicItemWriter(databaseClient);
+        writer.setBatchSize(10);
+
+        return stepBuilderFactory.get("step1")
+                .<String, DocumentWriteOperation>chunk(10)
+                .reader(reader)
+                .processor(processor)
+                .writer(writer)
+                .build();
+    }
+
+}
